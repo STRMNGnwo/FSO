b66eeea62417c89edae8a8240d105b9ee9291343
Srinivas Ilancheran <srinivasilancheran@gmail.com>
2021-06-14 17:57:09 +0100

    Submission of Exercise 1.7 which involved more minor calculations

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -18,21 +18,30 @@ function Button(props) { //Button Component, props include function handle and t
   
   }//end of Button Component.
 
+  var totalFeedback=0; //global variable that represents TotalFeedback
+  
 function App() {
 
   const[good, setGood]=useState(0);
   const[neutral,setNeutral]=useState(0);
   const[bad,setBad]=useState(0);
 
-  const addGoodReview=()=>{ //helper functions to change  state indirectly through button press.
+  var averageFeedback=(good-bad)/totalFeedback;
 
+  const addGoodReview=()=>{ //helper functions to change  state indirectly through button press.
+    totalFeedback++;
     setGood(good+1);
+    
   }
   const addNeutralReview=()=>{
+    totalFeedback++;
     setNeutral(neutral+1);
+    
   }
   const addBadReview=()=>{
+    totalFeedback++;
     setBad(bad+1);
+    
   }
   return (
     <>
@@ -44,6 +53,9 @@ function App() {
     <p>good:{good}</p> 
     <p>neutral:{neutral}</p>
     <p>bad:{bad} </p>
+    <p>Total Feedback: {totalFeedback}</p>
+    <p>Average Feedback: {averageFeedback||0}</p>
+    <p>Positive Feedback: {((good/totalFeedback)*100)||0}%</p>
     
 
 
