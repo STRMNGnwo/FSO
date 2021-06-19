ac4ee950a639264f0efd8ca2f87566c4d5dbf4bc
Srinivas Ilancheran <srinivasilancheran@gmail.com>
2021-06-19 17:51:59 +0100

    Submission of exercise 2.13, which involved displaying detailed information about a country, when a corresponding button is clicked

--- Part2/dataforcountries/src/App.js
+++ Part2/dataforcountries/src/App.js
@@ -1,12 +1,13 @@
 import React,{useState,useEffect}from 'react';
 import List from "./components/List";
+import Button from "./components/Button"
 import axios from 'axios';
 
 function App() {
 
   const[displayCountries,setDisplayCountries]=useState([]); //this state variable is used to store the list of countries retrieved.
   const[searchString, setSearchString] =useState(''); //this state variable is used to store the value of the searchString obtained from input element.
-
+  const[displayView,setDisplayView]=useState([]); //this state is used to display the country's detailed information when button is pressed. 
   useEffect(()=>{
     const url="https://restcountries.eu/rest/v2/all";
 
@@ -21,7 +22,10 @@ function App() {
   
     console.log("handling search field");
     setSearchString(event.target.value);
+    setDisplayView([]); //reseting displayView to nothing.
   }
+
+  
   var validSearchString=true;
 
   if(searchString==='') validSearchString=false;
@@ -29,11 +33,24 @@ function App() {
   //the line below would return every country, or only countries that contain the search String in their name.
   const countriesList=validSearchString ? displayCountries.filter((country)=>((country.name).toUpperCase()).includes(searchString.toUpperCase()) ) :displayCountries;
    
-  //the line below results in a string being printed out or a list of country names. It consists of a nested ternary operator. Based on fulfillment of conditions, certain things are displayed.
-  const displayList=
-  countriesList.length>10 ? "Please be more specific with you search, there are too many possibilities" :
-(countriesList.length===1)? countriesList.map((country)=><List key={country.name} name={country.name} capital={country.capital} population={country.population} languages={country.languages}/>)                                           :
-                            countriesList.map((country)=><List key={country.name} name={country.name}/>);
+  const handleButtonClick=(country)=>{ //outer function takes in a country object as a  parameter.
+     
+    console.log("Handling click");
+    //inner function actually displays the new view.
+    const displayCountry=()=>{
+    const display=<List key={country.name} name={country.name} capital={country.capital} population={country.population} languages={country.languages}/>;
+    
+    console.log(display);
+
+    setDisplayView(display);
+    }
+
+    return displayCountry; //outer function returns the inner function
+
+  }
+  //the line below results in a string being printed out or a list of country names.A ternary operator is used.
+  const displayList=countriesList.length>10 ? "Please be more specific with your search, there are too many possibilities" :
+  countriesList.map((country)=><><List key={country.name} name={country.name}/> <Button functionHandle={handleButtonClick(country)} text="Show!"/> </>);
   
   
 
@@ -43,6 +60,8 @@ function App() {
     <input id="searchField" value={searchString} name="searchField" onChange={handleSearchField}/>
     <br></br>
      <ul>   {displayList}  </ul>
+
+     <ul>{displayView}</ul>
     
     
     </>
@@ -84,4 +103,10 @@ const handleSearchField=(event)=>{    //used to handle onChange calls from the i
 
 
 
-//Inefficient way of printing: <li key={country.name}><h1>{country.name}</h1> <br/> Capital={country.capital} <br/> Population:{country.population }<h2>Languages</h2> </li>)       
+/* Ternary nested to meet all conditions for part 1 of data for countries
+ const displayList=
+  countriesList.length>10 ? "Please be more specific with you search, there are too many possibilities" :
+(countriesList.length===1)? countriesList.map((country)=><List key={country.name} name={country.name} capital={country.capital} population={country.population} languages={country.languages}/>)                                           :
+                            countriesList.map((country)=><List key={country.name} name={country.name}/>);
+  
+    */
